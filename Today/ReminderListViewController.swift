979eeaf2090c1f6438dc7900543b223fdde3ef67
toaster <ko8au3s9b21@gmail.com>
2022-02-12 23:58:19 +0900

    today

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -9,31 +9,28 @@ import UIKit
 
 class ReminderListViewController: UITableViewController {
 
+    private var reminderListDataSource: ReminderListDataSource?
+
+    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Self.showDetailSegueIdentifier,
+           let destination = segue.destination as? ReminderDetailViewController,
+           let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            let reminder = Reminder.testData[indexPath.row]
+            destination.configure(with: reminder)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reminderListDataSource = ReminderListDataSource()
+        tableView.dataSource = reminderListDataSource
+    }
 }
 
 extension ReminderListViewController {
-    static let reminderListCellIdentifier = "ReminderListCell"
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier,for: indexPath) as? ReminderListCell else {
-            fatalError("Unable to dequeue ReminderCell")
-        }
-
-        let reminder = Reminder.testData[indexPath.row]
-        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)
-        cell.doneButtonAction = {
-            //ここで切り替えの処理を行うアクションを作ってる
-            Reminder.testData[indexPath.row].isComplete.toggle()
-            tableView.reloadRows(at: [indexPath], with: .none)
-        }
-        return cell
-    }
 }
 
